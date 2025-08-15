b738ceeb5231123254774bb92fbe0d1ae718333d
yoon5450 <dbseodnd356@naver.com>
2025-08-15 19:18:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# React+Vite+Supabase+Prettier+ESLint 초기 설정 프로젝트
+
+vite, supabase.js, prettier, eslint, react-icon 설치
+kind-tiger 설치
+
+@ : alias 설정
+
+ESLINT 규칙 조정
+```
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/prop-types": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+```
+
+### .gitignore 세팅
+
+### husky staged lint 적용
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url';
+// https://vite.dev/config/
+
+const viteConfig = defineConfig({
+  base: '/',
+  server: {
+    host: 'localhost',
+    port: 3000,
+    open: false,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
+
+export default viteConfig;
